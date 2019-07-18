f58dcc5e529c9400d27790a7b6b27d075cf70cb3
Brandon Liles <brandon.liles@gmail.com>
2019-07-18 12:37:55 -0400

    Build the array of paths from the context.pages

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ module.exports = (options, context) => ({
   async generated() {
     const { outDir } = context
     const manifestFilePath = path.resolve(outDir, 'manifest.json')
+
+    // build an array of the page paths
+    var paths = [];
+    for(var idx in context.pages) {
+        var page = context.pages[idx];
+        paths.push(page.path);
+    }
+
     await fs.writeFile(
       manifestFilePath,
-      JSON.stringify(context.pages)
+      JSON.stringify(paths)
     )
   }
 })
